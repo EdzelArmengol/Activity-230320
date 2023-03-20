7e9193aa5775faef81f534d6dadba6c18f7ae30c
EdzelArmengol <aee0880@dlsud.edu.ph>
2023-03-20 23:51:59 +0800

    Add activity code

---
+++ indexActivity.js
@@ -0,0 +1,21 @@
+console.log("Hello, Welcome to De La Salle University!");
+
+let firstname = "Edzel Angelo"
+let lastname = "Armengol"
+let age = 20
+let hobbies = ["Playing Guitar","Playing Basketball","Cooking/Baking", "Playing Games", "Enjoying Life"] 
+let homeaddress = {
+	houseNumbner: "Block 26 Lot 24",
+	street: "Kasoy street",
+	subdivision: "Cuevasville subdivision",
+	barangay: "Molino 4",
+	city: "Bacoor",
+	province: "Cavite",
+	zipCode: "4102"
+}
+
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+console.log(hobbies);
+console.log(homeaddress);
